34f739f8a6211511eec2795666a8310ef2b3d36c
Kyle MacMillan <macattackftw@gmail.com>
2018-09-26 10:42:43 -0600

    Finished 3c, skipping the rest of 3 for the time being.

--- HW1/problem3.cpp
+++ HW1/problem3.cpp
@@ -3,15 +3,15 @@
  *
  *              sum = 0 + 1 + -2 + 3 + -4...
  *
- * Compile: g++ -Wall -fopenmp -o problem3 problem2.cpp -std=c++11 -lm
-            g++ -Wall -fopenmp -o problem3 problem2.cpp -DDEBUG -std=c++11
+ * Compile: g++ -Wall -fopenmp -o problem3 problem3.cpp -std=c++11 -lm
+            g++ -Wall -fopenmp -o problem3 problem3.cpp -DDEBUG -std=c++11
  * Run:     ./problem3 
  *
  * Input:   none
  * Output:  Times for each of the three runs
  *
  * Notes:
- *    1.    You probably want to reduce x if running in DEBUG mode.
+ *    1.    
  *
  */
 
@@ -25,7 +25,11 @@ using namespace std::chrono;
 
 // Global
 uint8_t     thrds   = omp_get_num_procs();
-uint64_t x = 34359738368;
+#ifdef DEBUG
+    int64_t x = 6400;
+#else
+    int64_t x = 34359738368;
+#endif
 high_resolution_clock::time_point t1 = high_resolution_clock::now();
 high_resolution_clock::time_point t2 = high_resolution_clock::now();
 duration<double> run_time = duration_cast<duration<double>>\
@@ -34,35 +38,45 @@ duration<double> run_time = duration_cast<duration<double>>\
 
 void problem3_a();
 void problem3_b();
+void problem3_c();
+int64_t foo(int64_t i);
 
 int main(int argc, char* argv[]){
     
-    printf("Problem 3a:\n");
-    problem3_a();
+    // printf("Problem 3a:\n");
+    // problem3_a();
+    // printf("\nProblem 3b:\n");
+    // problem3_b();
+    // printf("Cannot be done in parallel as is.\n");
+    printf("Problem 3c:\n");
+    problem3_c();
 
 
+    printf("\nDone with problem 3...\n");
     return 0;
 }
 
 void problem3_a(){
     
-    uint64_t a[(long) sqrt(x)] = {};
-    uint64_t b[(long) sqrt(x)] = {};
+    uint64_t len = (long) sqrt(x);
+    uint64_t a[len] = {};
+    uint64_t b[len] = {};
 
-    uint64_t i = 0;
+    int64_t i = 0;
+    
 
     // Sequential sanity check
     t1 = high_resolution_clock::now();
-    for(i=0; i < (long) sqrt(x); i++) {
+    for(i = 0; i < (long) sqrt(x); ++i) {
         a[i] = 2.3 * i;
         if (i < 10) 
             b[i] = a[i];
     }
     t2 = high_resolution_clock::now();
     run_time = duration_cast<duration<double>>(t2 - t1);
-    for(uint64_t j = 0; j < (long) sqrt(x); ++j){
+    for(int64_t j = 0; j < (long) sqrt(x); ++j){
 #ifdef DEBUG
-        printf("a[%d]: %" PRIu16 "  \tb[%d]: %" PRIu16 "\n", j, a[j], j, b[j]);
+        printf("a[%" PRIi64"]: %" PRIu64 "  \tb[%" PRIi64 "]: %" PRIu64 "\n", j, a[j], j, b[j]);
 #endif
         a[j] = 0;
         b[j] = 0;
@@ -72,16 +86,16 @@ void problem3_a(){
     t1 = high_resolution_clock::now();
     // OMP implementation
     #pragma omp for
-    for(i=0; i < (long) sqrt(x); i++) {
+    for(i = 0; i < (long) sqrt(x); ++i) {
         a[i] = 2.3 * i;
         if (i < 10) 
             b[i] = a[i];
     }
     t2 = high_resolution_clock::now();
     run_time = duration_cast<duration<double>>(t2 - t1);
-    for(uint64_t j = 0; j < (long) sqrt(x); ++j){
+    for(int64_t j = 0; j < (long) sqrt(x); ++j){
 #ifdef DEBUG
-        printf("a[%d]: %" PRIu16 "  \tb[%d]: %" PRIu16 "\n", j, a[j], j, b[j]);
+        printf("a[%" PRIi64"]: %" PRIu64 "  \tb[%" PRIi64 "]: %" PRIu64 "\n", j, a[j], j, b[j]);
 #endif
         a[j] = 0;
         b[j] = 0;
@@ -97,23 +111,25 @@ void problem3_b(){
     //     if ( a[i] < b[i])
     //         flag = 1;
     // }
-    uint64_t a[(long) sqrt(x)] = {};
-    uint64_t b[(long) sqrt(x)] = {};
+    int64_t n = x;
+    uint64_t a[n] = {};
+    uint64_t b[n] = {};
 
-    uint64_t i = 0;
+    int64_t i = 0;
+    int8_t flag = 0;
 
     // Sequential sanity check
     t1 = high_resolution_clock::now();
-    for(i=0; i < (long) sqrt(x); i++) {
+    for(i = 0; (i * flag) < n; ++i) {
         a[i] = 2.3 * i;
-        if (i < 10) 
-            b[i] = a[i];
+        if ( a[i] < b[i])
+            flag = 1;
     }
     t2 = high_resolution_clock::now();
     run_time = duration_cast<duration<double>>(t2 - t1);
-    for(uint64_t j = 0; j < (long) sqrt(x); ++j){
+    for(int64_t j = 0; j < n; ++j){
 #ifdef DEBUG
-        printf("a[%d]: %" PRIu16 "  \tb[%d]: %" PRIu16 "\n", j, a[j], j, b[j]);
+        printf("a[%" PRIu64"]: %" PRIu64 "  \tb[%" PRIu64 "]: %" PRIu64 "\n", j, a[j], j, b[j]);
 #endif
         a[j] = 0;
         b[j] = 0;
@@ -123,16 +139,17 @@ void problem3_b(){
     t1 = high_resolution_clock::now();
     // OMP implementation
     #pragma omp for
-    for(i=0; i < (long) sqrt(x); i++) {
+    for(i = 0; i < n; ++i) {
         a[i] = 2.3 * i;
-        if (i < 10) 
-            b[i] = a[i];
+        if ( a[i] < b[i])
+            flag = 1;
     }
+
     t2 = high_resolution_clock::now();
     run_time = duration_cast<duration<double>>(t2 - t1);
-    for(uint64_t j = 0; j < (long) sqrt(x); ++j){
+    for(int64_t j = 0; j < n; ++j){
 #ifdef DEBUG
-        printf("a[%d]: %" PRIu16 "  \tb[%d]: %" PRIu16 "\n", j, a[j], j, b[j]);
+        printf("a[%" PRIi64"]: %" PRIu64 "  \tb[%" PRIi64 "]: %" PRIu64 "\n", j, a[j], j, b[j]);
 #endif
         a[j] = 0;
         b[j] = 0;
@@ -141,9 +158,60 @@ void problem3_b(){
 }
 
 
-// c.
-// for(i=0; i<n; i++) 
-// a[i] = foo(i);
+int64_t foo(int64_t i){
+    return i + i;
+}
+
+
+void problem3_c(){
+    // c.
+    // for(i=0; i < n; ++i) 
+        // a[i] = foo(i);
+#ifdef DEBUG    
+    int64_t n = 12;
+#else
+    int64_t n = 600000;
+#endif
+    uint64_t a[n] = {};
+    int64_t i = 0;
+
+    // Sequential sanity check
+    t1 = high_resolution_clock::now();
+    for(i=0; i < n; ++i) {
+        // printf("i: %" PRIi64 "\n", i);
+        a[i] = foo(i);
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIu64 "\n", j, a[j]);
+#endif
+        a[j] = 0;
+    }
+    printf("No OMP time : %.14f seconds\n", run_time.count());
+
+    t1 = high_resolution_clock::now();
+    // OMP implementation
+    #pragma omp for
+    for(i=0; i < n; ++i) {
+        a[i] = foo(i);
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIu64 "\n", j, a[j]);
+#endif
+        a[j] = 0;
+    }
+    printf("OMP time    : %.14f seconds\n", run_time.count());
+}
+
+
+
+
+
 // d.
 // for(i=0; i<n; i++) {
 // a[i] = foo(i);
