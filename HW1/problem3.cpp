0a6e68b62e7202ef1d42e12c9c7d4a2a9b8c793c
Kyle MacMillan <macattackftw@gmail.com>
2018-09-26 22:04:25 -0600

    Couple more knocked out for Problem 3. Two more to go in Problem 3.

--- HW1/problem3.cpp
+++ HW1/problem3.cpp
@@ -40,7 +40,10 @@ void problem3_a();
 void problem3_b();
 void problem3_c();
 void problem3_d();
-
+void problem3_e();
+void problem3_f();
+void problem3_g();
+void problem3_h();
 int64_t foo(int64_t i);
 
 int main(int argc, char* argv[]){
@@ -52,8 +55,16 @@ int main(int argc, char* argv[]){
     // printf("Cannot be done in parallel as is.\n");
     // printf("Problem 3c:\n");
     // problem3_c();
-    printf("Problem 3d:\n");
-    problem3_d();
+    // printf("Problem 3d:\n");
+    // problem3_d();
+    // printf("Problem 3e:\n");
+    // problem3_e();
+    // printf("Problem 3f:\n");
+    // problem3_f();
+    printf("Problem 3g:\n");
+    problem3_g();
+    printf("Problem 3h:\n");
+    problem3_h();
 
     printf("\nDone with problem 3...\n");
     return 0;
@@ -252,7 +263,7 @@ void problem3_d(){
     #pragma omp for
     for(i = 0; i < n; ++i) {
         a[i] = foo(i);
-        if(a[i] < b[i]) 
+        if(a[i] < b[i])
             a[i] = b[i];
     }
     t2 = high_resolution_clock::now();
@@ -267,15 +278,113 @@ void problem3_d(){
 }
 
 
+
+void problem3_e(){
 // e.
-// for(i=0; i<n; i++) {
-// a[i] = foo(i);
-// if(a[i] < b[i]) break;
+// for(i = 0; i < n; i++) {
+//     a[i] = foo(i);
+//     if(a[i] < b[i])
+//         break;
 // }
+#ifdef DEBUG    
+    int64_t n = 12;
+#else
+    int64_t n = 60000;
+#endif
+    uint64_t a[n] = {};
+    uint64_t b[n] = {};
+    int64_t i = 0;
+
+    // Sequential sanity check
+    t1 = high_resolution_clock::now();
+    for(i = 0; i < n; ++i) {
+        a[i] = foo(i);
+        if(a[i] < b[i])
+            break;
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIu64 "\n", j, a[j]);
+#endif
+        a[j] = 0;
+    }
+    printf("No OMP time : %.14f seconds\n", run_time.count());
+
+    t1 = high_resolution_clock::now();
+    // OMP implementation
+    #pragma omp for
+    for(i = 0; i < n; ++i) {
+        a[i] = foo(i);
+        if(a[i] < b[i])
+            break;
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIu64 "\n", j, a[j]);
+#endif
+        a[j] = 0;
+    }
+    printf("OMP time    : %.14f seconds\n", run_time.count());
+}
+
+
+
+
+
+void problem3_f(){
 // f.
 // dotp = 0;
-// for(i=0; i<n; i++)
-// dotp += a[i]*b[i];
+// for(i = 0; i < n; i++)
+//     dotp += a[i] * b[i];
+#ifdef DEBUG    
+    int64_t n = 12;
+#else
+    int64_t n = 60000;
+#endif
+    uint64_t a[n] = {};
+    uint64_t b[n] = {};
+    int64_t i = 0;
+
+    // Sequential sanity check
+    t1 = high_resolution_clock::now();
+    int64_t dotp = 0;
+    for(i = 0; i < n; i++){
+        dotp += a[i] * b[i];
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIi64 "\n", j, dotp);
+#endif
+        a[j] = 0;
+    }
+    printf("No OMP time : %.14f seconds\n", run_time.count());
+
+    t1 = high_resolution_clock::now();
+    // OMP implementation    
+    dotp = 0;
+    #pragma omp for
+    for(i = 0; i < n; i++){
+        dotp += a[i] * b[i];
+    }
+    t2 = high_resolution_clock::now();
+    run_time = duration_cast<duration<double>>(t2 - t1);
+    for(int64_t j = 0; j < n; ++j){
+#ifdef DEBUG
+        printf("a[%" PRIi64 "]: %" PRIi64 "\n", j, dotp);
+#endif
+        a[j] = 0;
+    }
+    printf("OMP time    : %.14f seconds\n", run_time.count());
+}
+
+
+
 // g.
 // for(i=k; i<2*k; i++)
 // a[i] = a[i] + a[i
