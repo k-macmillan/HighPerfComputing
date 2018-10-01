19919e70a3bf440ce050b9e60d02cd67f1d7013c
Kyle MacMillan <macattackftw@gmail.com>
2018-09-30 20:16:40 -0600

    Fixed unsigned integer overflowing to zero and causing an infinite loop. Have to be more mindful of that.

--- Prog1/Prog1_1.c
+++ Prog1/Prog1_1.c
@@ -4,18 +4,31 @@
 #include <stdbool.h>    // Booleans
 
 
-uint8_t EXTRACT_BIT(uint16_t n, uint8_t i);
+uint8_t check_circuit (int id, uint16_t z);
 void PrintV(uint8_t *v);
+uint16_t EXTRACT_BIT(uint16_t n, uint16_t i);
+
 
 int main(int argc, char **argv){
-    uint16_t arg = 65535u;
-    uint8_t v[16];
+    const uint16_t MAX_VAL = 65535;
+    uint16_t sum = 0;
 
-    for (uint8_t i = 0; i < 16; ++i){
-        v[i] = EXTRACT_BIT(arg, i);
+    // Timings
+    double begin = 0.0;
+    double end = 0.0;
+    double time = 0.0;
+    uint16_t runs = 1;
+
+    begin = omp_get_wtime();
+    for (uint16_t k = 0; k < runs; ++k){
+        #pragma omp parallel for num_threads(omp_get_num_procs()) schedule(static, 1) reduction(+: sum)
+        for (uint32_t i = 0; i < MAX_VAL; ++i){
+            sum += check_circuit(omp_get_thread_num(), i);
+        }
     }
-
-    PrintV(v);
+    end = omp_get_wtime();
+    time = (end - begin) * 1000;
+    printf("Found %" PRIu16 " valid combinations in %f ms.\n", sum / runs, time / runs);
 }
 
 void PrintV(uint8_t *v){
@@ -25,11 +38,11 @@ void PrintV(uint8_t *v){
 }
 
 /* Check if a given input produces TRUE (a one) */
-int check_circuit (int id, int z){
-    int v[16];        /* Each element is a bit of z */
-    int i;
-    for (i = 0; i < 16; i++){
-        v[i] = EXTRACT_BIT(z,i);
+uint8_t check_circuit (int id, uint16_t z){
+    uint8_t v[16];
+
+    for (uint8_t i = 0; i < 16; ++i){
+        v[i] = EXTRACT_BIT(z, i);
     }
     if ((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3]) && 
         (!v[3] || !v[4]) && (v[4] || !v[5]) && (v[5] || !v[6]) && 
@@ -54,4 +67,4 @@ int check_circuit (int id, int z){
  * In arg:    n, i
  * Out arg:   1 or 0
  */
-uint8_t inline EXTRACT_BIT(uint16_t n, uint8_t i){return n & 1 << i ? 1 : 0;}
+uint16_t inline EXTRACT_BIT(uint16_t n, uint16_t i){return n & 1 << i ? 1 : 0;}
