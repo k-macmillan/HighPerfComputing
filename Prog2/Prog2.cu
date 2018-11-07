48a13fb0b550c20736cf6b9d60f5677a6a39a8d6
Kyle MacMillan <kyle.macmillan@mines.sdsmt.edu>
2018-11-06 21:07:38 -0700

    Need to figure out how to deal with 2D arrays in CUDA, this isn't compiling or working at all.

--- Prog2/Prog2.cu
+++ Prog2/Prog2.cu
@@ -3,12 +3,15 @@
 #include <unistd.h>
 
 
-const uint16_t BLK_SIZE = 256;
-void sanityCheck();
-bool arrayCheck(const float *a1, const float *a2, const uint32_t &size);
-void printArray(const float *a1, const uint32_t &size);
+const uint16_t BLK_SIZE = 3;
 
-__global__ void matDotVec(const float *a1, const uint32_t &size);
+void sanityCheck();
+bool arrayCheck(const float *a1, const float *a2, const uint16_t &size);
+bool matrixCheck(float **m1, float **m2, const uint16_t &size);
+void printArray(const float *arr, const uint16_t &size);
+void printMatrix(float **arr, const uint16_t &size);
+
+__global__ void matDotVec(float **arr, const uint16_t &size);
 
 int main(int argc, char **argv){
     sanityCheck();
@@ -19,23 +22,27 @@ int main(int argc, char **argv){
 
 void sanityCheck()
 {
-    const uint32_t n = 3;
-    float testnxn[n][n];
+    uint16_t n = BLK_SIZE;
+
+    float **ptr = NULL;
+    float testnxn[BLK_SIZE][BLK_SIZE];
+    ptr = testnxn;
     float incr = 0.0;
-    for (uint8_t i = 0; i < n; ++i){
-        for (uint8_t j = 0; j < n; ++j){
+    for (uint16_t i = 0; i < n; ++i){
+        for (uint16_t j = 0; j < n; ++j){
             testnxn[i][j] = ++incr;
         }
     }
     float testnx1[n] = {10.0f, 11.0f, 12.0f};
 
     float *d_result;
-    float *h_result;
+    float **h_result;
     float *expected;
     uint32_t bytes = n * sizeof(float);
+    uint32_t bytes_sqrd = bytes * bytes;
 
-    h_result = (float*)malloc(bytes);
-    memset(h_result, 0.0f, bytes);
+    h_result = (float**)malloc(bytes_sqrd);
+    memset(h_result, 0.0f, bytes_sqrd);
 
     expected = (float*)malloc(bytes);
     expected[0] = 68.0f;
@@ -43,31 +50,32 @@ void sanityCheck()
     expected[2] = 266.0f;
     
 
-    cudaMalloc((float**)&d_result, bytes);
-    cudaMemcpy(d_result, h_result, bytes, cudaMemcpyHostToDevice);
+    cudaMalloc((float**)&d_result, bytes_sqrd);
+    cudaMemcpy(d_result, testnxn, bytes_sqrd, cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_result, expected, bytes, cudaMemcpyHostToDevice);
 
     // Call CUDA function here...
-    matDotVec<<<1, n>>>(n);
+    matDotVec<<<1, n>>>(ptr, n);
 
-    cudaMemcpy(h_result, d_result, bytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_result, d_result, bytes_sqrd, cudaMemcpyDeviceToHost);
     cudaFree(d_result);
 
-    if (arrayCheck(expected, h_result, n)){
+    if (matrixCheck(h_result, h_result, n)){
         std::cout << "Sanity check PASSED!" << std::endl;
     }
     else{
         std::cout << "Sanity check FAILED!" << std::endl;
-        printArray(h_result, n);
+        
     }
-    
+    printMatrix(h_result, n);
     free(h_result);
     free(expected);
 }
 
 
 
-bool arrayCheck(const float *a1, const float *a2, const uint32_t &size){
-    for (uint32_t i = 0; i < size; ++i){
+bool arrayCheck(const float *a1, const float *a2, const uint16_t &size){
+    for (uint16_t i = 0; i < size; ++i){
         if (a1[i] != a2[i]){            
             return false;
         }
@@ -77,15 +85,34 @@ bool arrayCheck(const float *a1, const float *a2, const uint32_t &size){
 
 
 
-void printArray(const float *a1, const uint32_t &size){
-    for (uint32_t i = 0; i < 3; ++i){
-        std::cout << a1[i] << " ";
+bool matrixCheck(float **m1, float **m2, const uint16_t &size){
+    for (uint16_t i = 0; i < size * size; ++i){
+        if (m1[i] != m2[i]){            
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+void printArray(const float *arr, const uint16_t &size){
+    for (uint16_t i = 0; i < size; ++i){
+        std::cout << arr[i] << " ";
     }
     std::cout << std::endl;
 }
 
 
 
-__global__ void matDotVec(const float *a1, const uint32_t &size){
-    __syncthreads();
+void printMatrix(float **arr, const uint16_t &size){
+    for (uint16_t i = 0; i < size; ++i){
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+__global__ void matDotVec(float **arr, const uint16_t &size){
+    
 }
