5bae5e27340eb1574e27a8f10f5de58e64cc1dd6
Kyle MacMillan <macattackftw@gmail.com>
2018-11-27 19:08:07 -0700

    Created diagonal checking algorithm. Need to write test code.

--- final/board.cpp
+++ final/board.cpp
@@ -26,5 +26,17 @@ bool Board::validRow(){
 
 
 bool Board::validDiagonal(){
+    /*  For each element in queens if 
+     *  j_val - i_val + j == i_val or zero we are on a diagonal.
+     *  
+     */
+    for (uint32_t i = 0; i < n; ++i){
+        for (uint32_t j = i; j < n; ++j){
+            diff = queens[j] - queens[i] + j - i;
+            if (diff == 0 || diff == queens[i]){
+                return false;
+            }
+        }
+    }
     return true;
 }
